196e9a54a9f6768a8c855deb98af70d1cc84fd93
Maggie <muriithinyawira13@gmail.com>
2022-11-22 12:27:22 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 4;
+let num4 = 5;
+
+const multiply = (num1*num2);
+const random = Math.round(Math.random()*2);
+const mod = (num3%num4);
+const max = Math.max(5,10,15,20);
+
+
